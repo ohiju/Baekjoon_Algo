2880d988071251690b7903fe21b675cd7b93d5ae
Justin (HeeJoo Oh) <94172351+ohiju@users.noreply.github.com>
2024-06-16 22:45:23 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 0;
+    int sum = 0;
+
+    for (int i=0; i<5; i++) {
+        cin >> num;
+        sum += num * num;
+    }
+
+    cout << sum % 10;
+    return 0;
+}
