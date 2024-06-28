c5533229d7393b2e6dae1205cf76742c778a6273
Justin (HeeJoo Oh) <94172351+ohiju@users.noreply.github.com>
2024-06-28 09:27:14 +0900

    [Gold III] Title: 택배, Time: 1748 ms, Memory: 35236 KB -BaekjoonHub

---
+++ 백준/Gold/1719. 택배/택배.py
@@ -0,0 +1,46 @@
+import sys
+import heapq
+input=sys.stdin.readline
+
+n,m = map(int,input().split())
+INF = 10**9
+arr = [[] for _ in range(n+1)]
+
+def dijkstra(n):
+    q = []
+    heapq.heappush(q, (0,n))
+    dist[n] = (0,-1)
+
+    while q:
+        d,x = heapq.heappop(q)
+
+        if d < dist[x][0]:
+            continue
+            
+        for nd,nx in arr[x]:
+            new_d = nd + d
+            if nx == n or nx == x:
+                continue
+            if new_d < dist[nx][0]:
+                heapq.heappush(q, (new_d, nx))
+                if dist[x][1] == -1:
+                    dist[nx] = (new_d, nx)
+                else:
+                    dist[nx] = (new_d, dist[x][1])
+            
+for _ in range(m):
+    a,b,t = map(int,input().split())
+    arr[a].append((t,b))
+    arr[b].append((t,a))
+    
+# dist = [(INF,-1)] * (n+1)
+# dijkstra(1)
+for i in range(1,n+1):
+    dist = [(INF,-1)] * (n+1)
+    dijkstra(i)
+    for j in range(1,n+1):
+        if dist[j][1] == - 1:
+            print('-', end=' ')
+        else:
+            print(dist[j][1], end=' ')
+    print()
