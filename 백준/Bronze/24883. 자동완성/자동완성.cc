21951b8e9cc13233f27eb3a382f9ead5b9657415
Justin (HeeJoo Oh) <94172351+ohiju@users.noreply.github.com>
2024-07-08 21:52:13 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char A;
+
+    cin >> A;
+
+    if (A == 'N' || A == 'n') {
+        cout << "Naver D2";
+    } else {
+        cout << "Naver Whale";
+    }
+
+    return 0;
+}
