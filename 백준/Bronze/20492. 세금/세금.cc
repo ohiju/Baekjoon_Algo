ac711573c5e34f382e960606efeda5ed6ea402f4
Justin (HeeJoo Oh) <94172351+ohiju@users.noreply.github.com>
2024-07-10 22:44:10 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    
+    cout << (N * 78) / 100 << " " << (N * 8 / 10) + (N * 2 * 78 / 1000);
+    
+    return 0;
+}
